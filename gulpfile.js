c11eddc79c51b3562bd548b6941896a487bdc784
Albert <w.albert221@gmail.com>
2016-12-15 18:20:31 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      csso = require('gulp-csso'),
+      rename = require('gulp-rename');
+
+gulp.task('default', () => {
+    return gulp.src('src/style.scss')
+               .pipe(sass({
+                   outputStyle: 'expanded'
+               }).on('error', sass.logError))
+               .pipe(rename({
+                   basename: "nastoletni"
+                }))
+               .pipe(gulp.dest('./dist'))
+               .pipe(csso())
+               .pipe(rename({
+                   suffix: ".min"
+                }))
+               .pipe(gulp.dest('./dist'));
+});
